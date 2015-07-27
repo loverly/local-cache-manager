088c8c64b8b6eebb57a23739c2665d3f9db46aa3
Christopher Coles <christopher.coles@lover.ly>
2015-07-27 16:42:19 -0400

    WIP. Making some final changes.

--- tests/test.js
+++ tests/test.js
@@ -11,10 +11,21 @@ var opts = {name: 'localManager', debug: true};
 // mocks window.localStorage object
 var localStorageObj = {};
 var ls = localStorageObj.localStorage = {};
+//localStorageObj.maxPropLimit = 2;
+//localStorageObj.props = 0;
+
+// sets a limit on the number of items that can be added to the cache,
+// for garbage collecting purposes.
 
 // mocks localStorage public API setItem()
 localStorageObj.setItem = function(key, value) {
     ls[key] = value;
+    //localStorageObj.props += 1;
+
+    // sets limit on the number of items that can be added to the cache
+    //if (localStorageObj.props == localStorageObj.maxPropLimit) {
+    //    throw Error('Limit has been reached for cache.');
+    //}
 };
 
 // mocks localStorage public API getItem()
@@ -41,11 +52,13 @@ var c = new Cache(opts, localStorageObj, g, objS, setS);
 
 /**************** Tests to Validate Use Cases ******************/
 //set an object in the cache
-c.setObject('key-3', {4: 'a', 5: 'b', 6: 'd'}, 'mobile-articles');
-c.setObject('key-1', {1: 'a', 2: 'b', 3: 'c'}, 'mobile-articles');
-c.setObject('key-2', {7: 'a', 8: 'b', 9: 'c'}, 'mobile-articles');
 c.setObject('key-3', {1: 'a', 2: 'b', 3: 'd'}, 'articles');
 
+c.setObject('key-1', {1: 'a', 2: 'b', 3: 'c'}, 'mobile-articles');
+
+c.setObject('key-2', {7: 'a', 8: 'b', 9: 'c'}, 'mobile-articles');
+
+c.setObject('key-3', {4: 'a', 5: 'b', 6: 'd'}, 'mobile-articles');
 
 //var b = c.getObject('key-obj');
 //console.log('Cache, ', b);
@@ -83,7 +96,6 @@ for (var i = 0; i < 13; i++) {
 //set categorical sets in the cache
 c.setCategoricalSets('key-set', itemsToSet, 'items-to-set');
 
-console.log('cache', c.storageSpace.localStorage)
 
 //console.log(c.localStorageObj.localStorage)
 // get an object in the cache
@@ -93,16 +105,6 @@ console.log('cache', c.storageSpace.localStorage)
  //get Categorical Sets in the cache
 //var ll  = c.getCategoricalSets('key-sets');
 //console.log('Categorical sets, ', ll);
-
-var lsLimit = localStorageObj.maxPropLimit = 2;
-var lsLength = Object.keys(localStorageObj.localStorage).length;
-
-// sets a limit on the number of items that can be added to the cache,
-// for garbage collecting purposes.
-
-//if (lsLength >= lsLimit) {
-//    throw Error('Local Storage Limit has been reached, please be advised.')
-//}
 
 
 //var orderCachedById = {'28714': 0, '38445': 1};
